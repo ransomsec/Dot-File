73afaa743523a1ffb3c51658532cb6d367e0a308
ransomsec <ransomsec@d4rk.pc>
2023-01-12 14:02:13 +0530

    AstriNvim config file

---
+++ default_theme/highlights.lua
@@ -0,0 +1,8 @@
+return function(hl)
+  hl.CursorLine.bg = "#151d28"
+  hl.Comment.italic = true
+  hl.IndentBlanklineChar.fg = "#282828"
+  hl.IndentBlanklineContextChar.fg = "#3f3f3f"
+  hl.IncSearch.bg = "#24450d"
+  return hl
+end
